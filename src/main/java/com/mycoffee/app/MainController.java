0ab74c2bf1782d3e6ac022175b9e3e08c2669f0a
hadh93 <hoondog1993@gmail.com>
2023-04-11 14:52:54 -0500

    Index page controller placeholder added

--- src/main/java/com/mycoffee/app/MainController.java
+++ src/main/java/com/mycoffee/app/MainController.java
@@ -1,2 +1,15 @@
-package com.mycoffee.app;public class MainController {
+package com.mycoffee.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class MainController {
+    @GetMapping("/")
+    String index(){
+        return "Hello, World!";
+    }
+
 }
